d2678b7e4d74fa5a298523480dbd1413668146fe
Trey Wayne <treywayne95@gmail.com>
2025-07-29 15:26:31 -0500

    Changed astro config to custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://SatoriDotExe.github.io'
+    site: 'https://dbmt.net'
 });
