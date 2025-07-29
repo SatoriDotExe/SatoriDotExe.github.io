f3c71a148ee7ba093983062ad6c7fe457b3df9d2
Trey Wayne <treywayne95@gmail.com>
2025-07-29 14:30:28 -0500

    changed site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://satoridotexe.github.io'
+    site: 'https://SatoriDotExe.github.io'
 });
